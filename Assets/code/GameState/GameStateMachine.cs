ce7d3461b91697ebe68fbe604c76294d2ab3a741
ThomasWLand <thomasland2001@gmail.com>
2026-01-26 21:17:21 +0000

    added game state boilerplate, set up game states

---
+++ Assets/code/GameState/GameStateMachine.cs
@@ -0,0 +1,56 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+
+public abstract class GameState : MonoBehaviour
+{
+    protected GameStateMachine _stateMachine;
+    public virtual void InitState(GameStateMachine stateMachine)
+    {
+        this._stateMachine = stateMachine;
+    }
+    public virtual void EnterState(){}
+    public virtual void UpdateState(){}
+    public virtual void ExitState(){}
+}
+
+public class GameStateMachine : MonoBehaviour
+{
+    public GameState entryState;
+    public GameState[] allStates;
+    private GameState _currState = null;
+
+    void Start()
+    {
+        foreach(GameState state in allStates)
+        {
+            state.InitState(this);
+        }
+        this.SetState(entryState);
+    }
+
+    void Update()
+    {
+        GameState currState = this._currState;
+        if(currState != null)
+        {
+            currState.UpdateState();
+        }
+    }
+
+    public void SetState(GameState newState)
+    {
+        GameState currState = this._currState;
+        if(currState == newState || newState == null)
+        {
+            return;
+        }
+        if(currState != null)
+        {
+            currState.ExitState();
+        }
+        this._currState = newState;
+        newState.EnterState();
+        Debug.Log("state changed: " + this._currState.gameObject.name);
+    }
+}
