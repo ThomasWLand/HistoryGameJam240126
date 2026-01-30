dd668b0c3a60fb841d693b4d9f34128701aeac8b
ThomasWLand <thomasland2001@gmail.com>
2026-01-30 20:48:41 +0000

    merge fix, cut down GameOverState for now

--- Assets/code/GameState/GameOverState.cs
+++ Assets/code/GameState/GameOverState.cs
@@ -4,20 +4,11 @@ using UnityEngine;
 public class GameOverState : GameState
 {
     public GameState gameEnterState;
-    public TextMeshProUGUI gameOverButtonText;
-    public GameObject[] gameUI;
-    private bool _passed;
     public void SetPlayerPassed(bool passed)
     {
-        this._passed = passed;
     }
     public override void EnterState()
     {
-        gameOverButtonText.text = this._passed ? "Next Level" : "Retry Level";
-        foreach(GameObject gameObject in gameUI)
-        {
-            gameObject.SetActive(true);
-        }
     }
 
     public void EnterNextLevel()
@@ -30,9 +21,5 @@ public class GameOverState : GameState
 
     public override void ExitState()
     {
-        foreach(GameObject gameObject in gameUI)
-        {
-            gameObject.SetActive(false);
-        }
     }
 }
